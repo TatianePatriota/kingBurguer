2028d3df4d5a2e542ff6e4a27d85bf74c9219296
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-13 13:59:59 -0300

    edited button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,25 @@
 import styled from "styled-components";
-import { WhatsApp } from "./CommonIcons";
 
-export function Button({ title }: any): JSX.Element {
-  return (
-    <StyledButton>
-      <WhatsApp />
-      <a href="/">{title}</a>
-    </StyledButton>
-  );
-}
-
-export const StyledButton = styled.div`
+export const Button = styled.button`
   background-color: yellow;
   padding: 4px 20px;
   display: flex;
-  align-items: center;
   border-radius: 5px;
   margin-left: 13px;
+  line-height: 25px;
+  font-weight: 700;
+  border: none;
+`;
+
+export const LargeButton = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.red};
+  font-size: ${({ theme }) => theme.font.size.default}px;
+  line-height: 32px;
+  padding: 10px 20px;
+  margin-top: 10px;
+  margin-left: 0;
 
   a {
-    padding-left: 9px;
-    line-height: 25px;
-    font-weight: 700;
+    color: ${({ theme }) => theme.colors.white};
   }
 `;
