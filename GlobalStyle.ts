63d5e6234b85c9d33895bc1a6fac1a3413b1467e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-14 14:12:40 -0300

    add titles in GlobalStyle

--- GlobalStyle.ts
+++ GlobalStyle.ts
@@ -4,10 +4,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    background-color: #FAF3F2;
   }
 
   a, li {
     text-decoration: none;
     list-style: none;
   }
+  h1, h2, h3, h4, h5  {
+    margin: 0;
+  }
+
 `;
