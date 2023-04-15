9fc92571829e5720ca33329bf1295c3837eed304
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-15 20:14:33 -0300

    add new component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App";
 import { GlobalStyle } from "../GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./components/Themes";
+import { PersonalizedService } from "./components/PersonalizedService";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
+      <PersonalizedService />
     </ThemeProvider>
   </React.StrictMode>
 );
