1c51f3bfa7f4eb8c77b61bb00713e6e70b8a7e8b
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-18 19:47:07 -0300

    add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { HeaderComponent } from "./components/HeaderComponent";
+import { Menu } from "./components/Menu";
+import { PersonalizedService } from "./components/PersonalizedService";
+import { SpecialOffers } from "./components/SpecialOffers";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <HeaderComponent />
+      <SpecialOffers />
+      <Menu />
+      <PersonalizedService />
     </div>
   );
 }
