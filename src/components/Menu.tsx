aba693619967aa2ce83f6b5095300f5e769581fb
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-18 19:47:28 -0300

    add styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,77 +5,76 @@ import BuguerMenu from "../assets/images/BuguerMenu.png";
 export function Menu() {
   return (
     <StyledContainerMenu>
-      <StyledMenuText>
-        <h1>
-          Escolha o seu combo imperial, <span>peça agora!</span>
-        </h1>
-        <p>
-          Temos vários tipos de pratos para a nossa realeza, fique esperto
-          porque temos sempre promoção!
-        </p>
-        <LargeButton>
-          <a href="/">Ver Cardápio Completo</a>
-        </LargeButton>
-      </StyledMenuText>
+      <StyledMenuImage>
+        <StyledMenuText>
+          <h1>
+            Escolha o seu combo imperial, <span>peça agora!</span>
+          </h1>
+          <p>
+            Temos vários tipos de pratos para a nossa realeza, fique esperto
+            porque temos sempre promoção!
+          </p>
+          <LargeButton>
+            <a href="/">Ver Cardápio Completo</a>
+          </LargeButton>
+        </StyledMenuText>
+      </StyledMenuImage>
       <StyledMenuPrices>
-        <h2>Cardápio imperial | Burger</h2>
-        <div>
-          <h3>
-            Classic burger
-            .........................................................................
-            <span>R$49,99</span>
-          </h3>
-          <p>
-            Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-            Tomate, Alface, Servidor do pão de batata
-          </p>
-        </div>
-        <div>
-          <h3>
-            Special big burger
-            ..................................................................
-            <span>R$49,99</span>
-          </h3>
-          <p>
-            Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-            Tomate, Alface, Servidor do pão de batata
-          </p>
-        </div>
-        <div>
-          <h3>
-            special big burger
-            ..................................................................
-            <span>R$49,99</span>
-          </h3>
-          <p>
-            Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-            Tomate, Alface, Servidor do pão de batata
-          </p>
-        </div>
-        <div>
-          <h3>
-            Mexican burger
-            ......................................................................
-            <span>R$49,99</span>
-          </h3>
-          <p>
-            Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-            Tomate, Alface, Servidor do pão de batata
-          </p>
-        </div>
+        <StyledMenuPricesContent>
+          <h2>Cardápio imperial | Burger</h2>
+          <div>
+            <h3>
+              Classic burger
+              .........................................................................
+              <span>R$49,99</span>
+            </h3>
+            <p>
+              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
+              Tomate, Alface, Servidor do pão de batata
+            </p>
+          </div>
+          <div>
+            <h3>
+              Special big burger
+              ..................................................................
+              <span>R$49,99</span>
+            </h3>
+            <p>
+              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
+              Tomate, Alface, Servidor do pão de batata
+            </p>
+          </div>
+          <div>
+            <h3>
+              special big burger
+              ..................................................................
+              <span>R$49,99</span>
+            </h3>
+            <p>
+              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
+              Tomate, Alface, Servidor do pão de batata
+            </p>
+          </div>
+          <div>
+            <h3>
+              Mexican burger
+              ......................................................................
+              <span>R$49,99</span>
+            </h3>
+            <p>
+              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
+              Tomate, Alface, Servidor do pão de batata
+            </p>
+          </div>
+        </StyledMenuPricesContent>
       </StyledMenuPrices>
     </StyledContainerMenu>
   );
 }
 
-const StyledMenuText = styled.div`
-  background: url(${BuguerMenu});
-  height: 575px;
-  background-size: contain;
-  background-repeat: no-repeat;
-  position: absolute;
-  font-weight: 400;
-  width: 50%;
+export const StyledMenuText = styled.div`
+  padding: 180px 110px 0 375px;
+
   h1 {
     color: ${({ theme }) => theme.colors.brownBackground};
     text-transform: uppercase;
@@ -97,14 +96,24 @@ const StyledMenuText = styled.div`
   }
 `;
 
+const StyledMenuPricesContent = styled.div`
+  padding: 70px 375px 0 30px;
+`;
+
+const StyledMenuImage = styled.div`
+  background: url(${BuguerMenu});
+  height: 575px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  font-weight: 400;
+  width: 50%;
+`;
+
 const StyledMenuPrices = styled.div`
   background-color: ${({ theme }) => theme.colors.brownBackground};
   color: ${({ theme }) => theme.colors.menuColor};
-  position: absolute;
   height: 575px;
   width: 50%;
-  right: 0;
-  padding-left: 30px;
 
   h2 {
     color: ${({ theme }) => theme.colors.yellow};
@@ -113,8 +122,6 @@ const StyledMenuPrices = styled.div`
     font-family: ${({ theme }) => theme.fonts.fontLelita};
     line-height: 35px;
     margin-bottom: 15px;
-    padding-top: 70px;
-    padding-top: 15px;
   }
   h3 {
     color: ${({ theme }) => theme.colors.menuColor};
@@ -131,11 +138,12 @@ const StyledMenuPrices = styled.div`
     line-height: 22px;
     font-weight: 400;
     font-size: ${({ theme }) => theme.font.size.small}px;
+    margin: 0;
+    margin-bottom: 20px;
   }
 `;
 
-const StyledContainerMenu = styled.div`
-  position: relative;
+const StyledContainerMenu = styled.section`
+  display: flex;
+  justify-content: space-between;
 `;
-
-// const StyledMenuContent
