fa50f891bef0e24cd07bf8dcaf6de8978242f033
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-18 19:48:57 -0300

    add component

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
-      <PersonalizedService />
     </ThemeProvider>
   </React.StrictMode>
 );
