50c4526aab8f97389677fe9e47fde042141acf2b
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-22 19:06:55 -0300

    add map api in component

---
+++ src/components/LocalizationMap.tsx
@@ -0,0 +1,38 @@
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import styled from "styled-components";
+
+export function LocalizationMap() {
+  const position = { lat: -22.900781, lng: -42.036651 };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBNmgAvpnZk0i4Fdhd_IzmgeG2uzponY2o",
+  });
+  return (
+    <StyledLocalizationMap>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          center={position}
+          zoom={15}
+        >
+          <Marker
+            position={position}
+            options={{
+              label: {
+                text: "Empire Burguer",
+              },
+            }}
+          />
+          <></>
+        </GoogleMap>
+      ) : (
+        <></>
+      )}
+    </StyledLocalizationMap>
+  );
+}
+
+const StyledLocalizationMap = styled.div`
+  height: 200px;
+`;
