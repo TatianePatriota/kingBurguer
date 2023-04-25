b80f8102ff9ed4d32ce030a325e623534c7e5306
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-25 10:53:46 -0300

    add hooks of deliveries

---
+++ src/components/Deliveries-services.ts
@@ -0,0 +1,24 @@
+import { DeliveriesCardType, DeliveriesIcon } from "./DeliveriesCard";
+
+export const deliveriesOptions: DeliveriesCardType[] = [
+  {
+    title: "Whatsapp",
+    description: "Vamos direto ao ponto, sem perder tempo!",
+    icon: DeliveriesIcon.WhatsApp,
+  },
+  {
+    title: "Entrega",
+    description: "Entregamos menos de 45 minutos na porta da sua casa!",
+    icon: DeliveriesIcon.Delivery,
+  },
+  {
+    title: "Sobremesa",
+    description: "Pedidos assima de 100 reais, ganham brindes.",
+    icon: DeliveriesIcon.Dessert,
+  },
+  {
+    title: "iFood",
+    description: "Nossa loja é Top 1 da região!",
+    icon: DeliveriesIcon.IFood,
+  },
+];
