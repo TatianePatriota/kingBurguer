5e22a75e8d978fc23e519edad6446029a98b86c2
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-04-26 14:01:06 -0300

    add informations menu

---
+++ src/components/HeaderInformation.ts
@@ -0,0 +1,19 @@
+import { HeaderIcon, InformationType } from "./InformationHeader";
+
+export const informations: InformationType[] = [
+  {
+    title: "Artesanal",
+    description: "Nossas receitas são feitas com todo cuidado",
+    icon: HeaderIcon.Burguer,
+  },
+  {
+    title: "Atendimento",
+    description: "Totalmente personalizado",
+    icon: HeaderIcon.Fone,
+  },
+  {
+    title: "DELIVERY Speed",
+    description: "Entregamos menos de 45 minutos",
+    icon: HeaderIcon.Delivery,
+  },
+];
