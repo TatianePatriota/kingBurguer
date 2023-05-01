6ef26b239a9f2f66ca800d4c03bb5e38b20bcb28
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-01 17:25:27 -0300

    add type and menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,122 +1,63 @@
 import styled from "styled-components";
-import { LargeButton } from "./Button";
-import BuguerMenu from "../assets/images/BuguerMenu.png";
+import { ShowMenu } from "./ShowMenu";
+import { useEffect, useState } from "react";
+
+const toMoneyString = (raw: number) =>
+  Intl.NumberFormat("pt-BR", {
+    compactDisplay: "long",
+    currency: "BRL",
+    style: "currency",
+  }).format(raw);
+
+async function fetchMenu() {
+  return await fetch("/db.json")
+    .then((res) => res.json())
+    .then((res) => res.menu)
+    .catch(console.error);
+}
+type MenuOptionsType = {
+  title: string;
+  price: number;
+  description: string;
+};
 
 export function Menu() {
+  const [menu, setMenu] = useState<MenuOptionsType[] | undefined>();
+  useEffect(() => {
+    fetchMenu().then(setMenu);
+  }, []);
+
   return (
     <StyledContainerMenu>
-      <StyledMenuImage>
-        <StyledMenuText>
-          <h1>
-            Escolha o seu combo imperial, <span>peça agora!</span>
-          </h1>
-          <p>
-            Temos vários tipos de pratos para a nossa realeza, fique esperto
-            porque temos sempre promoção!
-          </p>
-          <LargeButton>
-            <a href="/">Ver Cardápio Completo</a>
-          </LargeButton>
-        </StyledMenuText>
-      </StyledMenuImage>
+      <ShowMenu
+        title="Escolha o seu combo imperial, peça agora!"
+        text="Temos vários tipos de pratos para a nossa realeza, fique esperto
+        porque temos sempre promoção!"
+        textLink="Ver Cardápio Completo"
+      />
       <StyledMenuPrices>
-        <StyledMenuPricesContent>
-          <h2>Cardápio imperial | Burger</h2>
-          <div>
+        <h2>Cardápio imperial | Burger</h2>
+        {menu?.map((item, index) => (
+          <div key={index}>
             <h3>
-              Classic burger
-              .........................................................................
-              <span>R$49,99</span>
+              {item.title}
+              <span>{toMoneyString(item.price)}</span>
             </h3>
-            <p>
-              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-              Tomate, Alface, Servidor do pão de batata
-            </p>
+            <p>{item.description}</p>
           </div>
-          <div>
-            <h3>
-              Special big burger
-              ..................................................................
-              <span>R$49,99</span>
-            </h3>
-            <p>
-              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-              Tomate, Alface, Servidor do pão de batata
-            </p>
-          </div>
-          <div>
-            <h3>
-              special big burger
-              ..................................................................
-              <span>R$49,99</span>
-            </h3>
-            <p>
-              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-              Tomate, Alface, Servidor do pão de batata
-            </p>
-          </div>
-          <div>
-            <h3>
-              Mexican burger
-              ......................................................................
-              <span>R$49,99</span>
-            </h3>
-            <p>
-              Hamburguer bonino 160g, Molho, Bacon, Queijo prato, peito de peru,
-              Tomate, Alface, Servidor do pão de batata
-            </p>
-          </div>
-        </StyledMenuPricesContent>
+        ))}
       </StyledMenuPrices>
     </StyledContainerMenu>
   );
 }
 
-export const StyledMenuText = styled.div`
-  padding: 180px 110px 0 375px;
-
-  h1 {
-    color: ${({ theme }) => theme.colors.brownBackground};
-    text-transform: uppercase;
-    font-size: ${({ theme }) => theme.font.gutter.large}px;
-    font-family: ${({ theme }) => theme.fonts.fontLelita};
-  }
-  span {
-    background-color: ${({ theme }) => theme.colors.yellow};
-  }
-  p {
-    font-size: ${({ theme }) => theme.font.size.small}px;
-    font-family: ${({ theme }) => theme.fonts.fontLato};
-    color: ${({ theme }) => theme.colors.titleLightBrown};
-  }
-
-  a {
-    font-weight: 700;
-    font-size: ${({ theme }) => theme.font.size.small}px;
-  }
-`;
-
-const StyledMenuPricesContent = styled.div`
-  padding: 70px 375px 0 30px;
-`;
-
-const StyledMenuImage = styled.div`
-  background: url(${BuguerMenu});
-  height: 575px;
-  background-size: contain;
-  background-repeat: no-repeat;
-  font-weight: 400;
-  width: 50%;
-`;
-
 const StyledMenuPrices = styled.div`
   background-color: ${({ theme }) => theme.colors.brownBackground};
   color: ${({ theme }) => theme.colors.menuColor};
-  height: 575px;
   width: 50%;
 
   h2 {
-    color: ${({ theme }) => theme.colors.yellow};
+    color: ${({ theme }) => theme.colors.backgroundYellow};
     text-transform: uppercase;
     font-size: ${({ theme }) => theme.font.gutter.default}px;
     font-family: ${({ theme }) => theme.fonts.fontLelita};
