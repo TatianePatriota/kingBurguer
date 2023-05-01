412d9029e2f6b260000e2542cc224ea8652f9da3
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-01 17:25:00 -0300

    add type and props

--- src/components/InformationHeader.tsx
+++ src/components/InformationHeader.tsx
@@ -1,54 +1,46 @@
 import styled from "styled-components";
-import { BurguerIcon, DeliveryIcon, FoneIcon } from "./CommonIcons";
+import { InformationHeaderIcons } from "./CommonIcons";
 
-export function InformationHeader(): JSX.Element {
+export enum HeaderIcon {
+  Burguer = "Burguer",
+  Fone = "Fone",
+  Delivery = "Delivery",
+}
+
+export type InformationType = {
+  title?: string;
+  description?: string;
+  icon: HeaderIcon;
+};
+
+export type CommonProps = {
+  informations: InformationType;
+};
+
+export function InformationHeader({ informations }: CommonProps): JSX.Element {
   return (
-    <StyledInformationHeader>
-      <StyledInformations>
-        <StyledBackgroundIcons>
-          <BurguerIcon />
-        </StyledBackgroundIcons>
-        <div>
-          <h3>Artesanal</h3>
-          <p>Nossas receitas são feitas com todo cuidado</p>
-        </div>
-      </StyledInformations>
-      <StyledInformations>
-        <StyledBackgroundIcons>
-          <FoneIcon />
-        </StyledBackgroundIcons>
-        <div>
-          <h3>Atendimento</h3>
-          <p>Totalmente personalizado</p>
-        </div>
-      </StyledInformations>
-      <StyledInformations>
-        <StyledInformations>
-          <StyledBackgroundIcons>
-            <DeliveryIcon />
-          </StyledBackgroundIcons>
-          <div>
-            <h3>DELIVERY Speed</h3>
-            <p>Entregamos menos de 45 minutos</p>
-          </div>
-        </StyledInformations>
-      </StyledInformations>
-    </StyledInformationHeader>
+    <StyledBackground>
+      <InformationHeaderIcons icon={informations.icon} />
+
+      <h3>{informations.title}</h3>
+      <p>{informations.description}</p>
+    </StyledBackground>
   );
 }
+
+const StyledBackground = styled.div`
+  margin: 0 auto;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 4px 40px rgba(179, 155, 132, 0.5);
+`;
 
 const StyledInformationHeader = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 15px 5px;
   width: 970px;
   font-weight: 400;
-  margin: -50px auto;
-
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.colors.white};
-  box-shadow: 0px 4px 40px rgba(179, 155, 132, 0.5);
 
   h3 {
     font-family: ${({ theme }) => theme.fonts.fontLelita};
@@ -69,11 +61,7 @@ const StyledInformationHeader = styled.section`
 `;
 
 const StyledInformations = styled.div`
-  display: flex;
-  align-items: center;
   border-right: 1px solid ${({ theme }) => theme.colors.lightGrey};
-  padding-left: 20px;
-  padding-right: 15px;
 
   :last-child {
     border: none;
