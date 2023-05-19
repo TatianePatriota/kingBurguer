884824e71b1960eb566e53fe2467be10bf29f5c1
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-19 17:45:40 -0300

    add new styles

--- src/components/Localization.tsx
+++ src/components/Localization.tsx
@@ -13,15 +13,23 @@ export function Localization({
 }: LocalizationType): JSX.Element {
   return (
     <StyledLocalization>
-      <StyledPublicationsText>
+      <StyledPublications>
         <h1>{title}</h1>
         <p>{description}</p>
         <LocalizationMap />
-      </StyledPublicationsText>
+      </StyledPublications>
     </StyledLocalization>
   );
 }
 
 const StyledLocalization = styled.div`
   padding-bottom: 30px;
+`;
+
+const StyledPublications = styled(StyledPublicationsText)`
+  @media (max-width: 428px) {
+    h1 {
+      font-size: ${({ theme }) => theme.font.gutter.small}px;
+    }
+  }
 `;
